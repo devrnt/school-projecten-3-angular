6b46783171e96671c43b3927ceff6d63c87f995b
JonathanDelm <jonathan.delmeiren.y1511@student.hogent.be>
2018-10-22 16:42:08 +0200

    Commit voor proberen van pullen van master naar branch

---
+++ src/app/components/richting-scherm/richting-scherm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-richting-scherm',
+  templateUrl: './richting-scherm.component.html',
+  styleUrls: ['./richting-scherm.component.css']
+})
+export class RichtingSchermComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
